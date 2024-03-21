93d010fc12c98c425f507419a8cc1cf3e6052005
MOHAMMED MUBBASHIR AHMED <97329674+MubbashirAhmed123@users.noreply.github.com>
2024-03-21 22:47:48 +0530

    deployed

--- src/Components/BookingConfirm.jsx
+++ src/Components/BookingConfirm.jsx
@@ -12,7 +12,6 @@ function BookingConfirm() {
   const len = allSlots.length
 
 
-
   const [display, setDisplay] = useState(false)
 
   const handleClick = () => {
@@ -34,18 +33,18 @@ function BookingConfirm() {
     <>
       <div className="container mx-auto flex justify-center ">
         {
-          [allSlots[len-1]] ? [allSlots[len-1]].map((ele, index) => (
-            <div key={index} className="mt-10 bg-gray-300 p-5 rounded shadow-md w-[350px]  text-gray-700 text-lg font-semibold ">
+          allSlots[len-1] ?
+            <div  className="mt-10 bg-gray-300 p-5 rounded shadow-md w-[350px]  text-gray-700 text-lg font-semibold ">
               
-              <h1 className="mb-3 ">Turf Name : <span className="font-bold text-gray-800 mx-5"> {ele?.turfName}</span></h1>
-              <h1 className="mb-3">Price per hr : <span className="font-bold text-gray-800 mx-5">{ele?.price}</span></h1>
-              <h1 className="mb-3">User name :  <span className="font-bold text-gray-800 mx-5">{ele?.name}</span></h1>
-              <h1 className="mb-3">From : <span className="font-bold text-gray-800 mx-5">{ele?.from && ele?.from.split('T')[1] > '12:00' ? ele?.from +' PM' : ele?.from+'AM'}</span></h1>
-              <h1 className="mb-3">To : <span className="font-bold text-gray-800 mx-5">{ele?.to && ele?.to.split('T')[1] > '12:00' ? ele?.to +' PM' : ele?.to+' AM'}</span></h1>
-              <h1 className="mb-3">Total Price To Pay : <span className="font-bold text-gray-800 mx-5">{ele?.totalPrice}</span></h1>
+              <h1 className="mb-3 ">Turf Name : <span className="font-bold text-gray-800 mx-5"> {allSlots[len-1].turfName}</span></h1>
+              <h1 className="mb-3">Price per hr : <span className="font-bold text-gray-800 mx-5">{allSlots[len-1].price}</span></h1>
+              <h1 className="mb-3">User name :  <span className="font-bold text-gray-800 mx-5">{allSlots[len-1].name}</span></h1>
+              <h1 className="mb-3">From : <span className="font-bold text-gray-800 mx-5">{allSlots[len-1].from && allSlots[len-1].from.split('T')[1] > '12:00' ? allSlots[len-1].from +' PM' : allSlots[len-1].from+'AM'}</span></h1>
+              <h1 className="mb-3">To : <span className="font-bold text-gray-800 mx-5">{allSlots[len-1].to && allSlots[len-1].to.split('T')[1] > '12:00' ? allSlots[len-1].to +' PM' : allSlots[len-1].to+' AM'}</span></h1>
+              <h1 className="mb-3">Total Price To Pay : <span className="font-bold text-gray-800 mx-5">{allSlots[len-1].totalPrice}</span></h1>
 
             </div>
-          )) : <div className="mt-5 bg-red-300 rounded p-2 w-full text-center font-bold">No Booking Yet</div>
+          : <div className="mt-5 bg-red-300 rounded p-2 w-full text-center font-bold">No Booking Yet</div>
         }
 
 
@@ -59,7 +58,7 @@ function BookingConfirm() {
      }
 
       {display && <motion.div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  z-10 w-full h-[100px] bg-red-200 p-2 rounded transition delay-700 bg-opacity-100 sm:w-[60%] lg:w-[40%]" initial={{top:-100,opacity:0}} animate={{top:50,opacity:1}}  >
-        <p className="font-bold">Are you want to sure you want to cancel the booking</p>
+        <p className="font-bold">Are you sure you want to cancel the booking?</p>
         <div className="flex justify-center gap-20 p-2 mt-3">
           <button className="bg-red-300 py-2 px-5 rounded hover:bg-red-400 transition" onClick={handleClick} >Yes</button>
           <button className="bg-green-300 py-2 px-5 rounded hover:bg-green-400 transition" onClick={() => setDisplay(false)}>No</button>
--- src/Components/BookingForm.jsx
+++ src/Components/BookingForm.jsx
@@ -23,7 +23,6 @@ function BookingForm({ selectedTurf }) {
     setDetails({ ...details, [name]: value })
   }
 
-  console.log(new Date(details.to),new Date(details.from))
   const timeDiff=Math.ceil((new Date(details.to)) - new Date(details.from))/(1000 * 60 *60)
   
   const handleSubmit = async (e) => {

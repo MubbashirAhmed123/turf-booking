f272bae3adb259462ade908c4d549eb67e28aefd
MOHAMMED MUBBASHIR AHMED <97329674+MubbashirAhmed123@users.noreply.github.com>
2024-03-22 18:15:36 +0530

    deployed

--- src/Components/BookedSlots.jsx
+++ src/Components/BookedSlots.jsx
@@ -1,9 +1,17 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import {  useDispatch, useSelector } from "react-redux"
 import { fetchdata } from "../store/turfSlice"
 function BookedSlots() {
 
   const { allSlots } = useSelector(state => state.turf)
+
+  const[selectedValue,setSelectedValue]=useState('')
+
+ const turfname=[...new Set(allSlots.map(turf => turf.turfName))]
+
+
+const filteredTufs= selectedValue!=='all' ? allSlots.filter((turf)=> turf.turfName===selectedValue 
+): allSlots
 
 
   const dispatch = useDispatch()
@@ -14,10 +22,18 @@ function BookedSlots() {
   }, [dispatch])
   return (
     <>
+    <form className="p-2 ">
+      <select name="" id="" className="p-2 rounded-md font-semibold bg-gray-200 outline-none focus:ring transition cursor-pointer" onChange={(e)=> setSelectedValue(e.target.value)}>
+        <option value="all" selected >All</option>
+        {turfname.map(ele=> <option value={ele}>{ele}</option> )}
+       
+      </select>
+    </form>
+
 
     <div className='p-2 ' >
         {
-            allSlots?.length>0 ? allSlots.map((ele,index)=>(
+            allSlots?.length>0 ? filteredTufs.map((ele,index)=>(
                 <ul key={index} className="p-2">
                   <li>Tur Name: {ele.turfName}</li>
                     <li>From: {ele.from.split('T')[1]>'12:00' ? ele.from+' PM' :ele.from+' AM'}</li>
